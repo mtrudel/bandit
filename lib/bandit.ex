3c6f50212c6ad2560f3858ddb3669326d3a8fca8
Mat Trudel <mat@geeky.net>
2019-11-18 16:20:33 -0500

    Update child_spec to harmonize with Plug.Cowboy's arguments

--- lib/bandit.ex
+++ lib/bandit.ex
@@ -4,35 +4,39 @@ defmodule Bandit do
   to start a Bandit server hosting a plug looks like:
 
   ```
-  {Bandit, plug: {MyApp.Plug, :arg_passed_to_plug_init}, opts: [port: 4000]}
+  {Bandit, plug: {MyApp.Plug, :arg_passed_to_plug_init}, options: [port: 4000]}
   ```
 
-  The third argument allows for the following options: 
+  Three options are supported:
 
-  `port`: The port to bind to. Defaults to 4000
-  `num_acceptors`: The number of acceptor processes to run. This is mostly a performance
-  tuning knob and can usually be left at the default value of 10
-  `transport_options`: A keyword list of options to be passed into the listener socket
+  * `scheme`: Currently only `:http` is supported
+  * `plug`: The plug to handle connections. Can be specified as `MyPlug` or `{MyPlug, plug_opts}`
+  * `options`: Options to pass to `ThousandIsland.Server`. For an exhaustive list of options see the 
+    `ThousandIsland.Server` documentation, however some common options are:
+      * `port`: The port to bind to. Defaults to 4000
+      * `num_acceptors`: The number of acceptor processes to run. This is mostly a performance
+      tuning knob and can usually be left at the default value of 10
+      * `listener_options`: A keyword list of options to be passed into the listener socket
   """
 
   def child_spec(arg) do
-    {opts, illegal_opts} =
+    {options, illegal_options} =
       arg
-      |> Keyword.get(:opts, [])
-      |> Keyword.split(~w(port num_acceptors transport_options)a)
+      |> Keyword.get(:options, [])
+      |> Keyword.split(~w(port num_acceptors listener_options)a)
 
-    if illegal_opts != [] do
-      raise "Unsupported option(s) in Bandit config: #{inspect(illegal_opts)}"
+    if illegal_options != [] do
+      raise "Unsupported option(s) in Bandit config: #{inspect(illegal_options)}"
     end
 
-    opts =
-      opts
+    options =
+      options
       |> Keyword.put(:handler_module, Bandit.Handler)
-      |> Keyword.put(:handler_opts, plug(arg))
+      |> Keyword.put(:handler_options, plug(arg))
 
     %{
       id: Bandit,
-      start: {ThousandIsland.Server, :start_link, [opts]}
+      start: {ThousandIsland.Server, :start_link, [options]}
     }
   end
 
@@ -40,7 +44,7 @@ defmodule Bandit do
     arg
     |> Keyword.fetch!(:plug)
     |> case do
-      {plug, plug_opts} -> {plug, plug.init(plug_opts)}
+      {plug, plug_options} -> {plug, plug.init(plug_options)}
       plug -> {plug, plug.init([])}
     end
   end
