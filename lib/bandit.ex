f13a58bffa2384353b53804c313bb27a4c75820b
Mat Trudel <mat@geeky.net>
2020-11-07 20:49:31 -0500

    Allow explicit specification of transport_module

--- lib/bandit.ex
+++ lib/bandit.ex
@@ -17,6 +17,9 @@ defmodule Bandit do
       * `port`: The port to bind to. Defaults to 4000
       * `num_acceptors`: The number of acceptor processes to run. This is mostly a performance
       tuning knob and can usually be left at the default value of 10
+      * `transport_module`: The name of the module which provides basic socket functions.
+      This overrides any value set for `scheme` and is intended for cases where control
+      over the socket at a fundamental is needed.
       * `transport_options`: A keyword list of options to be passed into the transport socket's listen function
   """
 
@@ -24,7 +27,7 @@ defmodule Bandit do
     {options, illegal_options} =
       arg
       |> Keyword.get(:options, [])
-      |> Keyword.split(~w(port num_acceptors transport_options)a)
+      |> Keyword.split(~w(port num_acceptors transport_module transport_options)a)
 
     if illegal_options != [] do
       raise "Unsupported option(s) in Bandit config: #{inspect(illegal_options)}"
@@ -38,7 +41,7 @@ defmodule Bandit do
 
     options =
       options
-      |> Keyword.put(:transport_module, transport_module)
+      |> Keyword.put_new(:transport_module, transport_module)
       |> Keyword.put(:handler_module, Bandit.Handler)
       |> Keyword.put(:handler_options, plug(arg))
 
