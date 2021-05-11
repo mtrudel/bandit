mtrudel/bandit

commit 67d678f3495fbd4dfae91c3d12cfcfc7fc2b2126

Improve behaviour of chunking

lib/bandit/conn_pipeline.ex
@@ -47,7 +47,6 @@ defmodule Bandit.ConnPipeline do
     case conn do
       %Plug.Conn{state: :unset} -> raise(Plug.Conn.NotSentError)
       %Plug.Conn{state: :set} -> Plug.Conn.send_resp(conn)
-      %Plug.Conn{state: :chunked, adapter: {adapter_mod, req}} -> adapter_mod.chunk(req, "")
       %Plug.Conn{} -> conn
       _ -> raise("Expected #{plug}.call/2 to return %Plug.Conn{} but got: #{inspect(conn)}")
     end

lib/bandit/http1_request.ex
@@ -20,7 +20,6 @@ defmodule Bandit.HTTP1Request do
   # credo:disable-for-this-file Credo.Check.Refactor.CyclomaticComplexity
   # credo:disable-for-this-file Credo.Check.Refactor.CondStatements
   # credo:disable-for-this-file Credo.Check.Refactor.Nesting
-  @dialyzer {:nowarn_function, chunk: 2}
 
   @impl Bandit.HTTPRequest
   def request(%Socket{} = socket, data), do: {:ok, __MODULE__, %__MODULE__{socket: socket, buffer: data}}
@@ -262,10 +261,10 @@ defmodule Bandit.HTTP1Request do
   end
 
   @impl Plug.Conn.Adapter
-  def chunk(%__MODULE__{socket: socket} = req, chunk) do
+  def chunk(%__MODULE__{socket: socket}, chunk) do
     byte_size = chunk |> byte_size() |> Integer.to_string(16)
     Socket.send(socket, [byte_size, "\r\n", chunk, "\r\n"])
-    {:ok, nil, req}
+    :ok
   end
 
   defp response_header(version, status, headers) do

commit 3fdf33c7457487dfe054c36363803ab31f7b4def

Fix regression introduced in 67d678f

lib/bandit/conn_pipeline.ex
@@ -55,10 +55,21 @@ defmodule Bandit.ConnPipeline do
 
   defp commit_response(conn, plug) do
     case conn do
-      %Plug.Conn{state: :unset} -> raise(Plug.Conn.NotSentError)
-      %Plug.Conn{state: :set} -> Plug.Conn.send_resp(conn)
-      %Plug.Conn{} -> conn
-      _ -> raise("Expected #{plug}.call/2 to return %Plug.Conn{} but got: #{inspect(conn)}")
+      %Plug.Conn{state: :unset} ->
+        raise(Plug.Conn.NotSentError)
+
+      %Plug.Conn{state: :set} ->
+        Plug.Conn.send_resp(conn)
+
+      %Plug.Conn{state: :chunked, adapter: {adapter_mod, req}} ->
+        adapter_mod.chunk(req, "")
+        conn
+
+      %Plug.Conn{} ->
+        conn
+
+      _ ->
+        raise("Expected #{plug}.call/2 to return %Plug.Conn{} but got: #{inspect(conn)}")
     end
   end
 