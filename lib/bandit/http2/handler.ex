690423035151510befc154972e4b1dca54f99330
Mat Trudel <mat@geeky.net>
2021-05-11 17:21:53 -0400

    Implement buffer growth handling when insufficient bytes available
    Mark pending frame and error handling with TODO

--- lib/bandit/http2/handler.ex
+++ lib/bandit/http2/handler.ex
@@ -5,20 +5,37 @@ defmodule Bandit.HTTP2.Handler do
 
   use ThousandIsland.Handler
 
+  alias Bandit.HTTP2.Frame
+
   @impl ThousandIsland.Handler
   def handle_connection(socket, state) do
     socket
     |> ThousandIsland.Socket.recv(24)
     |> case do
-      {:ok, "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"} -> {:ok, :continue, state}
+      {:ok, "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"} -> {:ok, :continue, Map.put(state, :buffer, <<>>)}
       {:ok, _} -> {:error, "Did not receive expected HTTP/2 connection preface", state}
       {:error, reason} -> {:error, reason, state}
     end
   end
 
   @impl ThousandIsland.Handler
-  def handle_data(_data, _socket, %{plug: _plug} = state) do
-    # TODO - implement HTTP/2
-    {:ok, :close, state}
+  def handle_data(data, _socket, %{buffer: buffer} = state) do
+    (buffer <> data)
+    |> Frame.parse()
+    |> case do
+      {:ok, nil, rest} ->
+        {:ok, :continue, %{state | buffer: rest}}
+
+      {:ok, _frame, rest} ->
+        # TODO - implement HTTP/2
+        {:ok, :continue, %{state | buffer: rest}}
+
+      {:more, rest} ->
+        {:ok, :continue, %{state | buffer: rest}}
+
+      {:error, _stream, _code, reason} ->
+        # TODO - improve error handling here
+        {:error, reason, state}
+    end
   end
 end
--- test/bandit/http2/request_test.exs
+++ test/bandit/http2/request_test.exs
@@ -5,6 +5,14 @@ defmodule HTTP2RequestTest do
 
   setup :https_server
 
+  describe "request handling" do
+    @tag :pending
+    test "it should handle cases where the request arrives in small chunks" do
+      # TODO - write out a request one byte at a time and ensure that it returns as expected
+      # We can't test for this until we get a complete end to end request working
+    end
+  end
+
   describe "malformed requests" do
     test "closes with an error if the HTTP/2 connection preface is not present", context do
       errors =
@@ -25,6 +33,19 @@ defmodule HTTP2RequestTest do
         end)
 
       assert errors =~ "Did not receive expected HTTP/2 connection preface"
+    end
+
+    @tag :pending
+    test "it should ignore unknown frame types" do
+      # TODO - write out an invalid frame type and ensure that we continue to work thereafter
+      # We can't test for this until we get a complete end to end request working
+    end
+
+    @tag :pending
+    test "it should shut down the connection gracefully when encountering a connection error" do
+      # TODO - write out an invalid SETTINGS frame and ensure that we see a GOAWAY frame with an 
+      # appropriate error code
+      # We can't test for this until we get a complete end to end request working
     end
   end
 end
