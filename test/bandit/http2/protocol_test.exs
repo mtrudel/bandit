480223c9e9a8d1fce471b03c5d056e9309ef9161
Mat Trudel <mat@geeky.net>
2021-05-12 21:53:02 -0400

    Refactor protocol level tests out from higher level request tests

---
+++ test/bandit/http2/protocol_test.exs
@@ -0,0 +1,62 @@
+defmodule HTTP2ProtocolTest do
+  use ConnectionHelpers, async: true
+
+  import ExUnit.CaptureLog
+
+  setup :https_server
+
+  describe "connection tests" do
+    @tag :pending
+    test "it should handle cases where the request arrives in small chunks" do
+      # TODO - write out a request one byte at a time and ensure that it returns as expected
+      # We can't test for this until we get a complete end to end request working
+    end
+
+    @tag :pending
+    test "it should handle cases where multiple frames arrive in the same packet" do
+      # TODO - write out a request with several small frames in the same packet and ensure that it
+      # returns as expected
+      # We can't test for this until we get a complete end to end request working
+    end
+
+    test "closes with an error if the HTTP/2 connection preface is not present", context do
+      errors =
+        capture_log(fn ->
+          tls_client = tls_client(context)
+          :ssl.send(tls_client, "PRI * NOPE/2.0\r\n\r\nSM\r\n\r\n")
+          {:error, :closed} = :ssl.recv(tls_client, 0)
+
+          # Let the server shut down so we don't log the error
+          Process.sleep(100)
+        end)
+
+      assert errors =~ "Did not receive expected HTTP/2 connection preface"
+    end
+
+    @tag :pending
+    test "it should ignore unknown frame types" do
+      # TODO - write out an invalid frame type and ensure that we continue to work thereafter
+      # We can't test for this until we get a complete end to end request working
+    end
+
+    @tag :pending
+    test "it should shut down the connection gracefully when encountering a connection error" do
+      # TODO - write out an invalid SETTINGS frame and ensure that we see a GOAWAY frame with an
+      # appropriate error code
+      # We can't test for this until we get a complete end to end request working
+    end
+  end
+
+  def tls_client(context) do
+    {:ok, tls_client} =
+      :ssl.connect(:localhost, context[:port],
+        active: false,
+        mode: :binary,
+        verify: :verify_none,
+        cacertfile: Path.join(__DIR__, "../../support/cert.pem"),
+        alpn_advertised_protocols: ["h2"]
+      )
+
+    tls_client
+  end
+end
--- test/bandit/http2/request_test.exs
+++ test/bandit/http2/request_test.exs
@@ -1,58 +1,22 @@
 defmodule HTTP2RequestTest do
   use ConnectionHelpers, async: true
 
-  import ExUnit.CaptureLog
+  import Plug.Conn
 
   setup :https_server
+  setup :http2_client
 
   describe "request handling" do
-    @tag :pending
-    test "it should handle cases where the request arrives in small chunks" do
-      # TODO - write out a request one byte at a time and ensure that it returns as expected
-      # We can't test for this until we get a complete end to end request working
+    test "it should run hello world", context do
+      {:ok, response} =
+        Finch.build(:get, context[:base] <> "/hello_world") |> Finch.request(context[:finch_name])
+
+      assert response.status == 200
+      assert response.body == "OK"
     end
 
-    @tag :pending
-    test "it should handle cases where multiple frames arrive in the same packet" do
-      # TODO - write out a request with several small frames in the same packet and ensure that it
-      # returns as expected
-      # We can't test for this until we get a complete end to end request working
-    end
-  end
-
-  describe "malformed requests" do
-    test "closes with an error if the HTTP/2 connection preface is not present", context do
-      errors =
-        capture_log(fn ->
-          {:ok, client} =
-            :ssl.connect(:localhost, context[:port],
-              active: false,
-              verify: :verify_none,
-              cacertfile: Path.join(__DIR__, "../../support/cert.pem"),
-              alpn_advertised_protocols: ["h2"]
-            )
-
-          :ssl.send(client, "PRI * NOPE/2.0\r\n\r\nSM\r\n\r\n")
-          {:error, :closed} = :ssl.recv(client, 0)
-
-          # Let the server shut down so we don't log the error
-          Process.sleep(100)
-        end)
-
-      assert errors =~ "Did not receive expected HTTP/2 connection preface"
-    end
-
-    @tag :pending
-    test "it should ignore unknown frame types" do
-      # TODO - write out an invalid frame type and ensure that we continue to work thereafter
-      # We can't test for this until we get a complete end to end request working
-    end
-
-    @tag :pending
-    test "it should shut down the connection gracefully when encountering a connection error" do
-      # TODO - write out an invalid SETTINGS frame and ensure that we see a GOAWAY frame with an
-      # appropriate error code
-      # We can't test for this until we get a complete end to end request working
+    def hello_world(conn) do
+      send_resp(conn, 200, "OK")
     end
   end
 end
