b9c60d6e18c430cfe0193bae695479e2830218d5
Mat Trudel <mat@geeky.net>
2021-05-13 21:58:33 -0400

    Be consistent with stream vs. stream_id

--- lib/bandit/http2/frame.ex
+++ lib/bandit/http2/frame.ex
@@ -6,19 +6,19 @@ defmodule Bandit.HTTP2.Frame do
   require Logger
 
   def deserialize(
-        <<length::24, type::8, flags::binary-size(1), _reserved::1, stream::31,
+        <<length::24, type::8, flags::binary-size(1), _reserved::1, stream_id::31,
           payload::binary-size(length), rest::binary>>
       ) do
     type
     |> case do
-      0x4 -> Frame.Settings.deserialize(flags, stream, payload)
-      0x6 -> Frame.Ping.deserialize(flags, stream, payload)
-      0x7 -> Frame.Goaway.deserialize(flags, stream, payload)
-      unknown -> handle_unknown_frame(unknown, flags, stream, payload)
+      0x4 -> Frame.Settings.deserialize(flags, stream_id, payload)
+      0x6 -> Frame.Ping.deserialize(flags, stream_id, payload)
+      0x7 -> Frame.Goaway.deserialize(flags, stream_id, payload)
+      unknown -> handle_unknown_frame(unknown, flags, stream_id, payload)
     end
     |> case do
       {:ok, frame} -> {{:ok, frame}, rest}
-      {:error, stream, code, reason} -> {{:error, stream, code, reason}, rest}
+      {:error, stream_id, code, reason} -> {{:error, stream_id, code, reason}, rest}
     end
   end
 
@@ -31,12 +31,14 @@ defmodule Bandit.HTTP2.Frame do
   end
 
   def serialize(frame) do
-    {type, flags, stream, payload} = Serializable.serialize(frame)
-    <<byte_size(payload)::24, type::8, flags::binary-size(1), 0::1, stream::31, payload::binary>>
+    {type, flags, stream_id, payload} = Serializable.serialize(frame)
+
+    <<byte_size(payload)::24, type::8, flags::binary-size(1), 0::1, stream_id::31,
+      payload::binary>>
   end
 
-  defp handle_unknown_frame(type, flags, stream, payload) do
-    "Unknown frame (t: #{type} f: #{inspect(flags)} s: #{stream} p: #{inspect(payload)})"
+  defp handle_unknown_frame(type, flags, stream_id, payload) do
+    "Unknown frame (t: #{type} f: #{inspect(flags)} s: #{stream_id} p: #{inspect(payload)})"
     |> Logger.warn()
 
     {:ok, nil}
