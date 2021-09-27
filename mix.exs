e968d60a4fc30ba67d9ac0c662396ef2f3fbc4fa
Mat Trudel <mat@geeky.net>
2021-09-27 11:47:49 -0400

    Version bump to 0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bandit.MixProject do
   def project do
     [
       app: :bandit,
-      version: "0.3.9",
+      version: "0.4.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
