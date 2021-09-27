5dc125bdf5c5acaf5089c0d420663a25fd19ac12
Mat Trudel <mat@geeky.net>
2021-09-27 15:02:55 -0400

    Use macros to generate error codes

--- lib/bandit/http2/errors.ex
+++ lib/bandit/http2/errors.ex
@@ -5,45 +5,25 @@ defmodule Bandit.HTTP2.Errors do
   @typedoc "An error code as defined for GOAWAY and RST_STREAM errors"
   @type error_code() :: non_neg_integer()
 
-  @spec no_error() :: error_code()
-  def no_error, do: 0x0
+  error_codes = %{
+    no_error: 0x0,
+    protocol_error: 0x1,
+    internal_error: 0x2,
+    flow_control_error: 0x3,
+    settings_timeout: 0x4,
+    stream_closed: 0x5,
+    frame_size_error: 0x6,
+    refused_stream: 0x7,
+    cancel: 0x8,
+    compression_error: 0x9,
+    connect_error: 0xA,
+    enhance_your_calm: 0xB,
+    inadequate_security: 0xC,
+    http_1_1_requires: 0xD
+  }
 
-  @spec protocol_error() :: error_code()
-  def protocol_error, do: 0x1
-
-  @spec internal_error() :: error_code()
-  def internal_error, do: 0x2
-
-  @spec flow_control_error() :: error_code()
-  def flow_control_error, do: 0x3
-
-  @spec settings_timeout() :: error_code()
-  def settings_timeout, do: 0x4
-
-  @spec stream_closed() :: error_code()
-  def stream_closed, do: 0x5
-
-  @spec frame_size_error() :: error_code()
-  def frame_size_error, do: 0x6
-
-  @spec refused_stream() :: error_code()
-  def refused_stream, do: 0x7
-
-  @spec cancel() :: error_code()
-  def cancel, do: 0x8
-
-  @spec compression_error() :: error_code()
-  def compression_error, do: 0x9
-
-  @spec connect_error() :: error_code()
-  def connect_error, do: 0xA
-
-  @spec enhance_your_calm() :: error_code()
-  def enhance_your_calm, do: 0xB
-
-  @spec inadequate_security() :: error_code()
-  def inadequate_security, do: 0xC
-
-  @spec http_1_1_requires() :: error_code()
-  def http_1_1_requires, do: 0xD
+  for {name, value} <- error_codes do
+    @spec unquote(name)() :: error_code()
+    def unquote(name)(), do: unquote(value)
+  end
 end
