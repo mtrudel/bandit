fa525cafd6ec9db4d6a35517ecb8de58e1d1624f
Mat Trudel <mat@geeky.net>
2022-09-28 16:56:14 -0400

    Fixup failing test

--- test/bandit/clock_test.exs
+++ test/bandit/clock_test.exs
@@ -1,13 +1,15 @@
 defmodule ClockTest do
   use ExUnit.Case, async: false
+
   import ExUnit.CaptureLog
+  import TestHelpers
 
   test "clock emits warning if ETS cache isn't available" do
     Application.stop(:bandit)
 
     warnings =
       capture_log(fn ->
-        date = Bandit.Clock.date_header()
+        {"date", date} = Bandit.Clock.date_header()
         assert valid_date_header?(date)
       end)
 
