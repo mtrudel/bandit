cf1379c43eba9066c7389def5a904b6e514b4160
Mat Trudel <mat@geeky.net>
2023-02-04 16:21:19 -0500

    Add explicit telemetry dependency

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Bandit.MixProject do
       {:plug, "~> 1.14"},
       {:websock, "~> 0.4.3"},
       {:hpax, "~> 0.1.1"},
+      {:telemetry, "~> 0.4 or ~> 1.0"},
       {:finch, "~> 0.8", only: [:dev, :test]},
       {:ex_doc, "~> 0.24", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
