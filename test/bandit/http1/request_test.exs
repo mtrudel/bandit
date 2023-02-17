977e83831ac08012b58d26246f006545b96f258a
Derek Kraan <derek@kraan.dev>
2023-02-17 17:36:17 +0100

    Skip this test until / when :erlang.decode_packet/3 adds support for ipv6 addresses.

--- test/bandit/http1/request_test.exs
+++ test/bandit/http1/request_test.exs
@@ -186,6 +186,7 @@ defmodule HTTP1RequestTest do
       assert Jason.decode!(body)["host"] == "banana"
     end
 
+    @tag :skip
     test "derives ipv6 host from the URI, even if it differs from host header", context do
       client = SimpleHTTP1Client.tcp_client(context)
 
