75f4ab4b9fef7a7b7822fd5220771364070c55db
Dan Schultzer <1254724+danschultzer@users.noreply.github.com>
2023-03-27 19:04:04 -0700

    Require Elixir 1.12 (#122)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bandit.MixProject do
     [
       app: :bandit,
       version: "0.7.4",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_path(Mix.env()),
