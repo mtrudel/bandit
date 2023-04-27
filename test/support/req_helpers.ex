c916170b32b090a5e56121164f3dfafeacdca3d0
Mat Trudel <mat@geeky.net>
2023-04-27 09:16:46 -0400

    Tweak req to use one finch pool per test

--- test/support/req_helpers.ex
+++ test/support/req_helpers.ex
@@ -4,27 +4,39 @@ defmodule ReqHelpers do
   defmacro __using__(_) do
     quote location: :keep do
       def req_http1_client(context) do
-        [req: context |> req_opts() |> Req.new()]
+        start_finch(context)
+        [req: build_req(context)]
       end
 
       def req_h2_client(context) do
-        [req: context |> req_opts() |> put_in([:connect_options, :protocol], :http2) |> Req.new()]
+        start_finch(context, protocol: :http2)
+        [req: build_req(context)]
       end
 
-      defp req_opts(context) do
-        [
+      defp start_finch(context, overrides \\ []) do
+        options =
+          [
+            conn_opts: [
+              transport_opts: [
+                verify: :verify_peer,
+                cacertfile: Path.join(__DIR__, "../support/ca.pem")
+              ]
+            ]
+          ]
+          |> Keyword.merge(overrides)
+
+        start_supervised!({Finch, name: context.test, pools: %{default: options}})
+      end
+
+      defp build_req(context) do
+        Req.new(
           base_url: context.base,
           retry: false,
           compressed: false,
           raw: true,
           follow_redirects: false,
-          connect_options: [
-            transport_opts: [
-              verify: :verify_peer,
-              cacertfile: Path.join(__DIR__, "../support/ca.pem")
-            ]
-          ]
-        ]
+          finch: context.test
+        )
       end
     end
   end
