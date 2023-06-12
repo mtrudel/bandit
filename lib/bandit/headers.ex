e0c8c0c30af9b0f57d8aafac37ddd9294363a8be
Ryan Winchester <ryanwinchester@users.noreply.github.com>
2023-06-12 10:41:37 -0300

    Manually parse integers in headers module (#148)

--- lib/bandit/headers.ex
+++ lib/bandit/headers.ex
@@ -1,6 +1,6 @@
 defmodule Bandit.Headers do
   @moduledoc false
-  # Conveniences for dealing with headers
+  # Conveniences for dealing with headers.
 
   @spec is_port_number(integer()) :: Macro.t()
   defguardp is_port_number(port) when Bitwise.band(port, 0xFFFF) === port
@@ -13,7 +13,7 @@ defmodule Bandit.Headers do
     end
   end
 
-  # covers ipv6 addresses, which look like this: `[::1]:4000` as defined in RFC3986
+  # Covers IPv6 addresses, like `[::1]:4000` as defined in RFC3986.
   @spec parse_hostlike_header(host_header :: binary()) ::
           {:ok, binary(), nil | integer()} | {:error, String.t()}
   def parse_hostlike_header("[" <> _ = host_header) do
@@ -21,7 +21,7 @@ defmodule Bandit.Headers do
     |> :binary.split("]:")
     |> case do
       [host, port] ->
-        case Integer.parse(port) do
+        case parse_integer(port) do
           {port, ""} when is_port_number(port) -> {:ok, host <> "]", port}
           _ -> {:error, "Header contains invalid port"}
         end
@@ -36,7 +36,7 @@ defmodule Bandit.Headers do
     |> :binary.split(":")
     |> case do
       [host, port] ->
-        case Integer.parse(port) do
+        case parse_integer(port) do
           {port, ""} when is_port_number(port) -> {:ok, host, port}
           _ -> {:error, "Header contains invalid port"}
         end
@@ -56,12 +56,9 @@ defmodule Bandit.Headers do
 
   @spec parse_content_length(binary()) :: {:ok, length :: integer()} | {:error, String.t()}
   defp parse_content_length(value) do
-    case Integer.parse(value) do
+    case parse_integer(value) do
       {length, ""} when length >= 0 ->
         {:ok, length}
-
-      {_length, ""} ->
-        {:error, "invalid negative content-length header (RFC9110§8.6)"}
 
       {length, rest} ->
         if rest |> Plug.Conn.Utils.list() |> all?(to_string(length)),
@@ -73,8 +70,23 @@ defmodule Bandit.Headers do
     end
   end
 
+  # Checks if all values in a list are equal to a given value.
   @spec all?([binary()], binary()) :: boolean()
   defp all?([value | rest], value), do: all?(rest, value)
   defp all?([], _str), do: true
   defp all?(_values, _value), do: false
+
+  # Parses non-negative integers from strings. Return the valid portion of an
+  # integer and the remaining string as a tuple like `{123, ""}` or `:error`.
+  def parse_integer(<<digit::8, rest::binary>>) when digit >= ?0 and digit <= ?9 do
+    parse_integer(rest, digit - ?0)
+  end
+
+  def parse_integer(_), do: :error
+
+  defp parse_integer(<<digit::8, rest::binary>>, total) when digit >= ?0 and digit <= ?9 do
+    parse_integer(rest, total * 10 + digit - ?0)
+  end
+
+  defp parse_integer(rest, total), do: {total, rest}
 end
