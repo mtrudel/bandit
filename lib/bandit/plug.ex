7d6f2203c70ada4a378be0db9ed20bc589e914cd
moogle19 <k.seidel@q1.eu>
2023-06-21 16:22:39 +0200

    Add typespec and simplify

--- lib/bandit/plug.ex
+++ lib/bandit/plug.ex
@@ -1,14 +1,13 @@
 defmodule Bandit.Plug do
   @moduledoc false
 
-  def to_peer_data({:local, path}, cert),
-    do: %{address: {:local, path}, port: 0, ssl_cert: cert}
+  @spec to_peer_data(ThousandIsland.Transport.socket_info(), :public_key.der_encoded() | nil) ::
+          Plug.Conn.Adapter.peer_data()
+  def to_peer_data({type, path}, cert) when type in [:local, :undefined],
+    do: %{address: {type, path}, port: 0, ssl_cert: cert}
 
   def to_peer_data({:unspec, <<>>}, cert),
     do: %{address: :unspec, port: 0, ssl_cert: cert}
-
-  def to_peer_data({:undefined, term}, cert),
-    do: %{address: {:undefined, term}, port: 0, ssl_cert: cert}
 
   def to_peer_data({ip, port}, cert), do: %{address: ip, port: port, ssl_cert: cert}
 end
