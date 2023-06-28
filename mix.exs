be1aca8f5b41ffdd54470de02d1ce7ade49c0591
Mat Trudel <mat@geeky.net>
2023-06-28 12:13:29 -0400

    Target Elixir 1.13+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bandit.MixProject do
     [
       app: :bandit,
       version: "1.0.0-pre.9",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_path(Mix.env()),
