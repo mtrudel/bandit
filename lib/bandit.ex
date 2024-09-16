2bb5a1857882bf324fd26ed7e095568c20a496a1
Mat Trudel <mat@geeky.net>
2024-09-16 16:22:00 -0400

    Update documentation

--- lib/bandit.ex
+++ lib/bandit.ex
@@ -109,12 +109,16 @@ defmodule Bandit do
   * `log_protocol_errors`: How to log protocol errors such as malformed requests. `:short` will
     log a single-line summary, while `:verbose` will log full stack traces. The value of `false`
     will disable protocol error logging entirely. Defaults to `:short`
+  * `log_client_closures`: How to log cases where the client closes the connection. These happen
+    routinely in the real world and so the handling of them is configured separately since they
+    can be quite noisy. Takes the same options as `log_protocol_errors`, but defaults to `false`
   """
   @type http_options :: [
           {:compress, boolean()}
           | {:deflate_opions, deflate_options()}
           | {:log_exceptions_with_status_codes, list() | Range.t()}
           | {:log_protocol_errors, :short | :verbose | false}
+          | {:log_client_closures, :short | :verbose | false}
         ]
 
   @typedoc """
