7016110f943b13f8f6917a3f9cf3f75cb2e4745b
Mat Trudel <mat@geeky.net>
2025-01-02 21:25:52 -0500

    Avoid collisions between finch pools for similarly named tests

--- test/support/req_helpers.ex
+++ test/support/req_helpers.ex
@@ -4,16 +4,18 @@ defmodule ReqHelpers do
   defmacro __using__(_) do
     quote location: :keep do
       def req_http1_client(context) do
-        start_finch(context)
-        [req: build_req(context)]
+        name = Module.concat(context.case, context.test)
+        start_finch(name)
+        [req: build_req(base_url: context.base, finch: name)]
       end
 
       def req_h2_client(context) do
-        start_finch(context, protocols: [:http2])
-        [req: build_req(context)]
+        name = Module.concat(context.case, context.test)
+        start_finch(name, protocols: [:http2])
+        [req: build_req(base_url: context.base, finch: name)]
       end
 
-      defp start_finch(context, overrides \\ []) do
+      defp start_finch(name, overrides \\ []) do
         options =
           [
             conn_opts: [
@@ -25,17 +27,14 @@ defmodule ReqHelpers do
           ]
           |> Keyword.merge(overrides)
 
-        start_supervised!({Finch, name: context.test, pools: %{default: options}})
+        start_supervised!({Finch, name: name, pools: %{default: options}})
       end
 
-      defp build_req(context) do
+      defp build_req(opts) do
         Req.Request.new([])
         |> Req.Request.append_request_steps(base_url: &Req.Steps.put_base_url/1)
         |> Req.Request.register_options([:base_url, :finch])
-        |> Req.Request.merge_options(
-          base_url: context.base,
-          finch: context.test
-        )
+        |> Req.Request.merge_options(opts)
       end
     end
   end
