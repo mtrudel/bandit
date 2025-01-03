5a7f37fdb160e7acf6820d1ef20b7a80af0b8c7e
Mat Trudel <mat@geeky.net>
2025-01-02 20:04:38 -0500

    Rename TestHelpers to DateHelpers

--- test/support/date_helpers.ex
+++ test/support/date_helpers.ex
@@ -1,4 +1,4 @@
-defmodule TestHelpers do
+defmodule DateHelpers do
   @moduledoc false
 
   @regex ~r/(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun), (?:[0-2][0-9]|3[0-1]) (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4} (?:[0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] GMT/
