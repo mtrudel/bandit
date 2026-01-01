49aac490fc5094f9c76189179790dd9a369e2905
Yuri S. <fudoshiki.ari@gmail.com>
2026-01-01 22:53:41 +0500

    Add missing `:zstd_options` key (#558)
    Signed-off-by: Yuri S. <fudoshiki.ari@gmail.com>

--- lib/bandit.ex
+++ lib/bandit.ex
@@ -239,7 +239,7 @@ defmodule Bandit do
   end
 
   @top_level_keys ~w(plug scheme port ip keyfile certfile otp_app cipher_suite display_plug startup_log thousand_island_options http_options http_1_options http_2_options websocket_options)a
-  @http_keys ~w(compress response_encodings deflate_options log_exceptions_with_status_codes log_protocol_errors log_client_closures)a
+  @http_keys ~w(compress response_encodings deflate_options zstd_options log_exceptions_with_status_codes log_protocol_errors log_client_closures)a
   @http_1_keys ~w(enabled max_request_line_length max_header_length max_header_count max_requests clear_process_dict gc_every_n_keepalive_requests log_unknown_messages)a
   @http_2_keys ~w(enabled max_header_block_size max_requests max_reset_stream_rate default_local_settings)a
   @websocket_keys ~w(enabled max_frame_size validate_text_frames compress deflate_options primitive_ops_module)a
