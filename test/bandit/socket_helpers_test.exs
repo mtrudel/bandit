b0fc468501df38136539dd02da48aa17fa26ae36
Mat Trudel <mat@geeky.net>
2026-01-05 19:59:06 -0500

    Minor preference for no aliasing

--- test/bandit/socket_helpers_test.exs
+++ test/bandit/socket_helpers_test.exs
@@ -1,15 +1,13 @@
 defmodule Bandit.SochetHeadersTest do
   use ExUnit.Case, async: true
 
-  alias Bandit.SocketHelpers, as: SH
-
   test "iodata_empty?" do
-    assert SH.iodata_empty?([])
-    assert SH.iodata_empty?("")
-    assert SH.iodata_empty?([["", []] | ""])
+    assert Bandit.SocketHelpers.iodata_empty?([])
+    assert Bandit.SocketHelpers.iodata_empty?("")
+    assert Bandit.SocketHelpers.iodata_empty?([["", []] | ""])
 
-    refute SH.iodata_empty?([1])
-    refute SH.iodata_empty?("1")
-    refute SH.iodata_empty?([["", []] | "ok"])
+    refute Bandit.SocketHelpers.iodata_empty?([1])
+    refute Bandit.SocketHelpers.iodata_empty?("1")
+    refute Bandit.SocketHelpers.iodata_empty?([["", []] | "ok"])
   end
 end
