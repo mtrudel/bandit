371a7fd5499f4bd40fa05b0d3ef05179a2902d7e
Thomas Cioppettini <544875+tomciopp@users.noreply.github.com>
2026-05-16 00:05:38 -0400

    fix(Warning): Logger.configure_backend/2 is deprecated.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,4 +2,4 @@ ExUnit.start(exclude: :slow)
 
 # Capture all logs so we're able to assert on logging done at info level in tests
 Logger.configure(level: :debug)
-Logger.configure_backend(:console, level: :warning)
+:logger.update_handler_config(:default, :level, :warning)
