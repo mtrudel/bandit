dedeb28c300e374b58c64824410bb466425c5471
Mat Trudel <mat@geeky.net>
2026-06-05 10:08:36 -0400

    Temporarily pin finch test dep until upstream issue fixed
    See https://github.com/sneako/finch/issues/375

--- mix.exs
+++ mix.exs
@@ -38,6 +38,8 @@ defmodule Bandit.MixProject do
       {:hpax, "~> 1.0"},
       {:telemetry, "~> 0.4 or ~> 1.0"},
       {:req, "~> 0.3", only: [:dev, :test]},
+      # finch pin is needed until https://github.com/sneako/finch/issues/375 gets resolved
+      {:finch, "< 0.22.0", only: [:dev, :test]},
       {:machete, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.24", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
